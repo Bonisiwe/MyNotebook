9ab4e030b5771d0f7546cf740fe698263a097769
Bonisiwe <1920451@students.wits.ac.za>
2021-07-24 21:04:53 +0200

    adding user to the database

--- app/src/main/java/com/example/mynotebook/MainActivity.java
+++ app/src/main/java/com/example/mynotebook/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.mynotebook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,16 +18,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String email ="65543@gmail.com";
+
+        String email ="16@gmail.com";
         String password ="1234556";
         auth = FirebaseAuth.getInstance();
         Register(email,password);
+
     }
 
     void Register(String email, String password){
         auth.createUserWithEmailAndPassword( email, password).addOnCompleteListener(task-> {
             if (task.isSuccessful()) {
-                Toast.makeText(this, "Hello world hello", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Hello world hello for reAL", Toast.LENGTH_SHORT).show();
+
+                User user = new User (email,password);
+
+                FirebaseDatabase.getInstance().getReference("Users")
+                        .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                        .setValue(user).addOnCompleteListener(tas -> {
+                            if(tas.isSuccessful()){
+                                Toast.makeText(this, "user registered", Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                Toast.makeText(this, "failed failed", Toast.LENGTH_SHORT).show();
+
+                            }
+                });
+
             } else {
                 Toast.makeText(this, "Nothing", Toast.LENGTH_SHORT).show();
             }
